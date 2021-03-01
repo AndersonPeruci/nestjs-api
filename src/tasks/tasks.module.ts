b8b079e606720da059c3439d55083f3dc0aeb562
AndersonAX4B <anderson.peruci@ax4b.com>
2021-03-01 16:58:31 -0300

    Conexão com o MongoDB

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose'
 import { Module } from '@nestjs/common';
 import { TaskService } from './shared/task.service';
 import { TasksController } from './tasks.controller';
+import { taskSchema } from './schemas/task.schema'
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{name: 'Task', schema: taskSchema}])
+    ],
     controllers: [TasksController],
     providers: [TaskService]
 })
